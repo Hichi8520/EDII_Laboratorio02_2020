b07547dd73b44df7fdd8ae9173b4ea8c933b75a7
Luis <hichi8520@gmail.com>
2020-02-10 15:18:15 -0600

    Plantilla lab

---
+++ Lab02_EDII/Models/Soda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab02_EDII.Models
+{
+    public class Soda
+    {
+        public string Name { get; set; }
+        public string Flavor { get; set; }
+        public int Volume { get; set; }
+        public double Price { get; set; }
+        public string Factory { get; set; }
+    }
+}
---
+++ Lab02_EDII/Models/Tree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab02_EDII.Models
+{
+    public class Tree
+    {
+    }
+}
